7a6707e2cc34242fc50dab1c9ace3e25ca9383a4
KjerstiDA <kjerstiaslesen@gmail.com>
2021-09-22 01:06:00 +0200

    update

--- eclipselink/jpa-basic/src/main/java/no/hvl/dat250/jpa/basicexample/Main.java
+++ eclipselink/jpa-basic/src/main/java/no/hvl/dat250/jpa/basicexample/Main.java
@@ -8,26 +8,25 @@ import javax.persistence.Persistence;
 import javax.persistence.Query;
 
 public class Main {
-    private static final String PERSISTENCE_UNIT_NAME = "todos";
+    private static final String PERSISTENCE_UNIT_NAME = "people";
     private static EntityManagerFactory factory;
 
     public static void main(String[] args) {
         factory = Persistence.createEntityManagerFactory(PERSISTENCE_UNIT_NAME);
         EntityManager em = factory.createEntityManager();
         // read the existing entries and write to console
-        Query q = em.createQuery("select t from Todo t");
-        List<Todo> todoList = q.getResultList();
-        for (Todo todo : todoList) {
-            System.out.println(todo);
+        Query q = em.createQuery("select t from Family t");
+        List<Family> familyList = q.getResultList();
+        for (Family family : familyList) {
+            System.out.println(family);
         }
-        System.out.println("Size: " + todoList.size());
+        System.out.println("Size: " + familyList.size());
 
-        // create new todo
+        // create new family
         em.getTransaction().begin();
-        Todo todo = new Todo();
-        todo.setSummary("This is a test");
-        todo.setDescription("This is a test");
-        em.persist(todo);
+        Family family = new Family();
+        family.setDescription("This is a test");
+        em.persist(family);
         em.getTransaction().commit();
 
         em.close();
